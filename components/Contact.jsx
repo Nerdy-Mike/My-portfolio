4058fb54e503fa1e85a17579024a5f9401c4fc85
Nerdy Mike <maule.hc@gmail.com>
2022-05-06 04:56:45 +0700

    add contact form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,50 +1,120 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Dialog, Combobox, Transition } from '@headlessui/react';
+import { send } from 'emailjs-com';
 import CloseButton from './buttons/CloseButton';
+import ContactMessage from './ContactMessage';
 
 function Contact({ isContactOpen, setIsContactOpen }) {
-  const handleSummit = (e) => {
-    e.preventDefault();
-    setIsContactOpen(false);
+  const [isMessageOpen, setIsMessageOpen] = useState(false);
+  const [toSend, setTosend] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+  const [status, setStatus] = useState('');
+  const handleChange = (e) => {
+    setTosend({
+      ...toSend,
+      [e.target.name]: e.target.value,
+    });
   };
 
+  const handleSummit = (e) => {
+    e.preventDefault();
+    if (toSend.name === '' || toSend.email === '' || toSend.subject === '' || toSend.message === '') {
+      setStatus('Please fill in all the fields.');
+      setIsMessageOpen(true);
+      return;
+    }
+    send(
+      'service_kojpcin',
+      'template_nmdk32a',
+      toSend,
+      '7aWjxE3_lfznRjU8K',
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        setStatus(response.status);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+    setIsContactOpen(false);
+    setIsMessageOpen(true);
+  };
   return (
+    <div>
+      <ContactMessage status={status} isOpen={isMessageOpen} setIsOpen={setIsMessageOpen} />
 
-    <Transition show={isContactOpen} as={Fragment}>
-      <Dialog
-        onClose={() => { setIsContactOpen(false); }}
-        className="fixed inset-0 z-50 overflow-auto p-4 pt-[20vh] 3xl:pt-[30vh]"
-      >
-        <Dialog.Overlay className="absolute inset-0 bg-gray-800/95" />
-        <div className=" flex flex-wrap items-center justify-center ">
-          <form className="w-11/12 md:w-9/12 bg-white z-10 p-10 relative" onSubmit={(e) => handleSummit()}>
-            <CloseButton closeModal={() => setIsContactOpen(false)} />
-            <div className="grid xl:grid-cols-2 xl:gap-6">
-              <div className="relative z-0 mb-6 w-full group">
-                <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your name</label>
-                <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+      <Transition show={isContactOpen} as={Fragment}>
+        <Dialog
+          onClose={() => { setIsContactOpen(false); }}
+          className="fixed inset-0 z-50 overflow-auto p-4 pt-[20vh] 3xl:pt-[30vh] "
+        >
+          <Dialog.Overlay className="absolute inset-0 bg-gray-600/95" />
+          <div className=" flex flex-wrap items-center justify-center ">
+
+            <form className="w-11/12 md:w-7/12 bg-gray-200 dark:bg-gray-800 z-10 p-10 relative rounded-md" onSubmit={(e) => handleSummit(e)}>
+              <div className="dark:text-gray-300 text-2xl pb-6">
+                Contact me
+              </div>
+              <CloseButton closeModal={() => setIsContactOpen(false)} />
+              <div className="grid xl:grid-cols-2 xl:gap-6">
+                <div className="relative z-0 mb-6 w-full group">
+                  <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your name</label>
+                  <input
+                    type="text"
+                    id="base-input"
+                    name="name"
+                    value={toSend.name}
+                    onChange={(e) => handleChange(e)}
+                    className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-md focus:outline-none  block w-full p-2.5 dark:bg-gray-700  dark:text-white "
+                  />
+                </div>
+                <div className="relative z-0 mb-6 w-full group">
+                  <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
+                  <input
+                    type="text"
+                    id="base-input"
+                    name="email"
+                    value={toSend.email}
+                    onChange={(e) => handleChange(e)}
+                    className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-md focus:outline-none  block w-full p-2.5 dark:bg-gray-700  dark:text-white "
+                  />
+                </div>
               </div>
               <div className="relative z-0 mb-6 w-full group">
-                <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
-                <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+                <input
+                  type="text"
+                  id="base-input"
+                  name="subject"
+                  value={toSend.subject}
+                  onChange={(e) => handleChange(e)}
+                  className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-md focus:outline-none  block w-full p-2.5 dark:bg-gray-700  dark:text-white "
+                />
               </div>
-            </div>
-            <div className="mb-6">
-              <label htmlFor="base-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-              <input type="text" id="base-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-            </div>
+              <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+              <textarea
+                id="message"
+                rows="4"
+                name="message"
+                value={toSend.message}
+                onChange={(e) => handleChange(e)}
+                className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-md focus:outline-none  block w-full p-2.5 dark:bg-gray-700  dark:text-white"
+                placeholder="Type away..."
+              />
+              <div className="flex justify-end pt-2">
+                <button type="submit" className=" text-gray-600 dark:text-white bg-teal-300 dark:bg-teal-600  font-medium rounded-md text-sm w-full sm:w-auto px-8 py-2.5 text-right mt-4">Submit</button>
+              </div>
+            </form>
+          </div>
 
-            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-            <textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type away..." />
-
-            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-4">Submit</button>
-          </form>
-        </div>
-
-      </Dialog>
-    </Transition>
-
+        </Dialog>
+      </Transition>
+    </div>
   );
 }
 
---
+++ components/ContactMessage.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useRef, Fragment } from 'react';
+import { Dialog, Combobox, Transition } from '@headlessui/react';
+
+function ContactMessage({ status, isOpen, setIsOpen }) {
+  return (
+    <Transition show={isOpen} as={Fragment}>
+      <Dialog
+        onClose={() => setIsOpen(false)}
+        className="absolute z-50 inset-x-0 top-0 flex w-screen items-center justify-center overflow-y-auto pt-[5vh] "
+      >
+        <Dialog.Overlay className="fixed inset-0  bg-gray-800/90 dark:bg-gray-500/70" />
+        <Transition.Child>
+          <div className="my-16  flex w-full max-w-sm transform flex-col items-center overflow-hidden rounded-lg bg-white dark:bg-slate-700 p-6 text-left align-middle shadow-xl">
+            <Dialog.Title as="h3" className="text-lg text-center font-medium leading-6 text-gray-900 dark:text-gray-100">
+
+              {status === 200
+                ? 'Message sent! I will get in touch with you shortly.'
+                : 'Sorry, something went wrong. Please try again later or send me an email directly.'}
+            </Dialog.Title>
+            {/* <div className="mt-2">
+              <p className="text-md text-gray-500 dark:text-gray-300">{status}</p>
+            </div> */}
+            <div className="mt-6 pl-6">
+              <button
+                type="button"
+                className="dark:bg-teal-600 px-8 inline-flex justify-center rounded-md border border-transparent bg-teal-200 mr-6 py-2 text-sm font-medium text-gray-900 hover:bg-primary-300  dark:text-slate-100 focus:outline-none"
+                onClick={() => setIsOpen(false)}
+              >
+                Okay
+              </button>
+            </div>
+          </div>
+        </Transition.Child>
+      </Dialog>
+    </Transition>
+  );
+}
+
+export default ContactMessage;
